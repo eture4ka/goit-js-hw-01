7fcf3e42585e7e398eec80f37352f8477f2305da
eture4ka <markijan07071984@gmail.com>
2026-04-30 18:53:49 +0300

    dd

--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,10 @@
     function getElementWidth(content, padding, border) {
-
+// ... Перетворюємо рядки "Npx" на числа
+const contentNum = Number.parseFloat(content);
+  const paddingNum = Number.parseFloat(padding);
+  const borderNum = Number.parseFloat(border);
     }
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
