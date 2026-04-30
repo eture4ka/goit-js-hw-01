57dc3c302e64087517b475db4b256ec0eeedafd8
eture4ka <markijan07071984@gmail.com>
2026-04-30 19:02:00 +0300

    вв

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,11 @@
 function getElementWidth(content, padding, border) {
-  // 1. Парсимо рядки у числа (це ви вже зробили добре)
   const contentNum = Number.parseFloat(content);
   const paddingNum = Number.parseFloat(padding);
   const borderNum = Number.parseFloat(border);
 
-  // 2. Обчислюємо загальну ширину та повертаємо її
-  // Формула: content + padding * 2 + border * 2
   return contentNum + paddingNum * 2 + borderNum * 2;
 }
 
-// 3. Перевірка (обов'язково залиште ці рядки)
 console.log(getElementWidth("50px", "8px", "4px"));     // 74
 console.log(getElementWidth("60px", "12px", "8.5px"));  // 101
 console.log(getElementWidth("200px", "0px", "0px"));   // 200
